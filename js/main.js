b29bca3b3f40bd5207244ea574ab199fc4788f39
Paul Rohrbeck <paul@paul-rohrbeck.de>
2014-09-21 15:10:21 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    // automatically submit form
+    $('#anet-form').submit();
+
+    // messages:
+    var successMessage = '<h2>Thank you!</h2><p class="alert alert-success"><span class="glyphicon glyphicon-envelope"></span> Thanks for your RSVP. See you on November 6!</p><p class="text-center"><img src="images/slideshow/2013_rayburn.jpg" alt=""  class="img-thumbnail register-success-img" /></p>';
+    var errorMessage = '<p class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> There was an error submitting your RSVP.</p>';
+
+});
